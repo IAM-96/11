19d4e7f143192b8b0d50efb42ee8a300e9d98e55
IAM-96 <56156953+IAM-96@users.noreply.github.com>
2019-11-01 19:14:38 +0300

    Initial commit

---
+++ app/src/main/java/com/ibrahim/disscussionboeard/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ibrahim.disscussionboeard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       final TextView txt1 = findViewById(R.id.txt1);
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.setText("Welcome");
+            }
+        });
+    }
+}
